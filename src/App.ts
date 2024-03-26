e0c72216ace2cc34b139c44cfaf06a9f15140fa7
Matheus Pacheco Cruz <pachecx35@gmail.com>
2024-03-26 08:34:24 -0300

    up

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,102 @@
 import styled from "styled-components";
 
+import BgDesk from './assets/pattern-bg-desktop.png';
+
 export const Container = styled.div`
+    min-width: 32rem;
+    width: 42rem;
+    padding: 0 2.4rem;
+    position: relative;
+    margin: 0 auto;
+   
+   .imgBgMobile{
+        position: absolute;
+        z-index: -1;
+        width: 100%;
+        left: 0;
+    }
+
+    @media(min-width: 760px){
+        width: 100%;
+        background-image: url('./assets/pattern-bg-desktop.png');
+
+        .imgBgMobile{
+            display: none;
+        }
+    }
+`;
+
+export const Search = styled.header`
+    text-align: center;
+    padding-top: 3rem;
+
+    .inputDiv{
+        margin: 3.4rem 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        input{
+            height: 4.8rem;
+            border: none;
+            border-radius: 1rem 0 0 1rem;
+            width: 100%;
+            font-size: 2rem;
+            padding-left: 1rem;
+            cursor: pointer;
+        }
+
+        button{
+            height: 4.8rem;
+            width: 5.8rem;
+            border: none;
+            background: black;
+            border-radius: 0 1rem 1rem 0;
+            cursor: pointer;
+        }
+
+        button:hover{
+            background: gray;
+            transition: .1s;
+        }
+        
+    }
+
+    h1{
+        color: #ffff;
+        font-size: 2rem;
+    }
+`;
+
+export const Card = styled.div`
+    background: #ffff;
+    padding: 2.5rem 2.5rem 1rem;
+    text-align: center;
+    border-radius: 1rem;
+
+    border: 1px solid red;
+
+    .info{
+        margin-bottom: 1.5rem;
+    }
+
+    .info span{
+        color: #939393;
+        font-size: 1.3rem;
+        font-weight: 500;
+    }
+
+    .info p{
+        font-size: 1.8rem;
+        font-weight: 500;
+    }
+   
+   @media(min-width: 760px){
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+
+        height: 16rem;
+    }
 `;
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,56 @@
-import { Container } from "./App"
+import { Container, Card, Search } from "./App"
+import mobileBg from './assets/pattern-bg-mobile.png';
+
+import iconArrow from './assets/icon-arrow.svg';
 
 function App() {
 
   return (
     <Container>
-      resge
+      <img 
+        className="imgBgMobile"
+        src={mobileBg} 
+        alt="" 
+      />
+
+      <Search>
+        <h1>IP Address Tracker</h1>
+        <div className="inputDiv">
+          <input
+            type="search"
+          />
+          <button>
+            <img
+              src={iconArrow}
+              alt=""
+            />
+          </button>
+        </div>
+      </Search>
+    
+      <Card>
+        <div className="info">
+          <span>IP ADRESS</span>
+          <p>192.101.303.250</p>
+        </div>
+
+        <div className="info">
+          <span>LOCATION</span>
+          <p>Brooklyn, NY 10001</p>
+        </div>
+        
+        <div className="info">
+          <span>TIME ZONE</span>
+          <p>UTC - 05:00</p>
+        </div>
+
+        <div className="info">
+          <span>ISP</span>
+          <p>SpaceX Starlink</p>
+        </div>
+      </Card>
+
+      
     </Container>
   )
 }
